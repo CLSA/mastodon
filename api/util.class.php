676237b89727d78c321fef08c001f6254e1d0ad8
Dean Inglis <inglisd@mcmaster.ca>
2012-01-05 12:00:55 -0500

    bug fixes, add logo, track vs cohort

---
+++ api/util.class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * util.class.php
+ * 
+ * @author Dean Inglis <inglisd@mcmaster.ca>
+ * @package mastodon
+ * @filesource
+ */
+
+namespace mastodon;
+
+/**
+ * util: utility class of static methods
+ *
+ * Extends cenozo's util class with additional functionality.
+ * @package mastodon
+ */
+class util extends \cenozo\util
+{
+}
+?>
