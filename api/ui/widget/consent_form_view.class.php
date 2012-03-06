5974bcfc8780ac9e01659aceb9d0ff0b0972075e
Patrick Emond <emondpd@mcmaster.ca>
2012-03-06 12:24:07 -0500

    Implementing base_form_view class and template

--- api/ui/widget/consent_form_view.class.php
+++ api/ui/widget/consent_form_view.class.php
@@ -15,7 +15,7 @@ use cenozo\lib, cenozo\log, mastodon\util;
  * 
  * @package mastodon\ui
  */
-class consent_form_view extends \cenozo\ui\widget\base_view
+class consent_form_view extends base_form_view
 {
   /**
    * Constructor
@@ -27,24 +27,24 @@ class consent_form_view extends \cenozo\ui\widget\base_view
    */
   public function __construct( $args )
   {
-    parent::__construct( 'consent_form', 'view', $args );
+    parent::__construct( 'consent_form', $args );
 
-    $this->add_item( 'invalid', 'boolean', 'Invalid' );
-  }
+    $this->add_item( 'option_1', 'Option #1' );
+    $this->add_item( 'option_2', 'Option #2' );
+    $this->add_item( 'date', 'Date' );
 
-  /**
-   * Finish setting the variables in a widget.
-   * 
-   * @author Patrick Emond <emondpd@mcmaster.ca>
-   * @access public
-   */
-  public function finish()
-  {
-    parent::finish();
+    // if there are two submitted entries for this form then display them both
+    $consent_form_entry_mod = lib::create( 'database\modifier' );
+    $consent_form_entry_mod->where( 'deferred', '=', false );
 
-    $this->set_item( 'invalid', $this->get_record()->invalid, true );
+    $consent_form_entry_list = 
+      $this->get_record()->get_consent_form_entry_list( $consent_form_entry_mod );
+    $db_consent_form_entry_1 = current( $consent_form_entry_list );
+    $db_consent_form_entry_2 = next( $consent_form_entry_list );
 
-    $this->finish_setting_items();
+    $this->set_form_entries(
+      false == $db_consent_form_entry_1 ? NULL : $db_consent_form_entry_1,
+      false == $db_consent_form_entry_2 ? NULL : $db_consent_form_entry_2 );
   }
 }
 ?>
