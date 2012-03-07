b4b31cfe48a8b4f5f6cfe0c2ed18ba675a610c36
Patrick Emond <emondpd@mcmaster.ca>
2012-03-07 13:33:16 -0500

    Creating another new base class.

---
+++ api/ui/widget/base_form_entry_list.class.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * base_form_entry_list.class.php
+ * 
+ * @author Patrick Emond <emondpd@mcmaster.ca>
+ * @package mastodon\ui
+ * @filesource
+ */
+
+namespace mastodon\ui\widget;
+use cenozo\lib, cenozo\log, mastodon\util;
+
+/**
+ * Base class for all form entry lists.
+ * 
+ * @package mastodon\ui
+ */
+class base_form_entry_list extends \cenozo\ui\widget\base_list
+{
+  /**
+   * Constructor
+   * 
+   * Defines all variables required by the form entry list.
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @param string $form_type The type of form being listed.
+   * @param array $args An associative array of arguments to be processed by the widget
+   * @access public
+   */
+  public function __construct( $form_type, $args )
+  {
+    parent::__construct( $form_type.'_form_entry', $args );
+    $this->form_type = $form_type;
+    
+    $this->add_column( $this->form_type.'_form_id', 'number', 'ID', true );
+    $this->add_column( 'date', $this->form_type.'_form.date', 'Date Added', false );
+  }
+  
+  /**
+   * Set the rows array needed by the template.
+   * 
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @access public
+   */
+  public function finish()
+  {
+    $form_id_name = $this->form_type.'_form_id';
+    $get_form_method = sprintf( 'get_%s_form', $this->form_type );
+
+    // the "add" function is overridden, so just make sure it gets included in the template
+    $this->set_addable( true );
+
+    parent::finish();
+    
+    foreach( $this->get_record_list() as $record )
+    {
+      $this->add_row( $record->id,
+        array( $this->form_type.'_form_id' => $record->$form_id_name,
+               'date' => $record->$get_form_method()->date ) );
+    }
+
+    $this->finish_setting_rows();
+  }
+
+  /**
+   * Overrides the parent class method to restrict form entry list based on user's role
+   * 
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @param database\modifier $modifier Modifications to the list.
+   * @return int
+   * @access protected
+   */
+  protected function determine_record_count( $modifier = NULL )
+  {
+    $type_id_name = $this->form_type.'_id';
+    $form_name = $this->form_type.'_form';
+    $db_user = lib::create( 'business\session' )->get_user();
+
+    if( is_null( $modifier ) ) $modifier = lib::create( 'database\modifier' );
+    $modifier->where( $form_name.'.invalid', '!=', true );
+    $modifier->where( $form_name.'.'.$type_id_name, '=', NULL );
+    $modifier->where( 'user_id', '=', $db_user->id );
+    $modifier->where( 'deferred', '=', true );
+
+    return parent::determine_record_count( $modifier );
+  }
+  
+  /**
+   * Overrides the parent class method to restrict form entry list based on user's role
+   * 
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @param database\modifier $modifier Modifications to the list.
+   * @return array( record )
+   * @access protected
+   */
+  protected function determine_record_list( $modifier = NULL )
+  {
+    $type_id_name = $this->form_type.'_id';
+    $form_name = $this->form_type.'_form';
+    $db_user = lib::create( 'business\session' )->get_user();
+
+    if( is_null( $modifier ) ) $modifier = lib::create( 'database\modifier' );
+    $modifier->where( $form_name.'.invalid', '!=', true );
+    $modifier->where( $form_name.'.'.$type_id_name, '=', NULL );
+    $modifier->where( 'user_id', '=', $db_user->id );
+    $modifier->where( 'deferred', '=', true );
+
+    return parent::determine_record_list( $modifier );
+  }
+
+  /**
+   * The type of form (ie: consent, contact, proxy)
+   * @var string $form_type;
+   * @access private
+   */
+  private $form_type;
+}
+?>
--- api/ui/widget/consent_form_entry_list.class.php
+++ api/ui/widget/consent_form_entry_list.class.php
@@ -15,87 +15,17 @@ use cenozo\lib, cenozo\log, mastodon\util;
  * 
  * @package mastodon\ui
  */
-class consent_form_entry_list extends \cenozo\ui\widget\base_list
+class consent_form_entry_list extends base_form_entry_list
 {
   /**
    * Constructor
-   * 
-   * Defines all variables required by the consent_form_entry list.
    * @author Patrick Emond <emondpd@mcmaster.ca>
    * @param array $args An associative array of arguments to be processed by the widget
    * @access public
    */
   public function __construct( $args )
   {
-    parent::__construct( 'consent_form_entry', $args );
-    
-    $this->add_column( 'consent_form_id', 'number', 'ID', true );
-    $this->add_column( 'date', 'consent_form.date', 'Date Added', false );
-  }
-  
-  /**
-   * Set the rows array needed by the template.
-   * 
-   * @author Patrick Emond <emondpd@mcmaster.ca>
-   * @access public
-   */
-  public function finish()
-  {
-    // the "add" function is overridden, so just make sure it gets included in the template
-    $this->set_addable( true );
-
-    parent::finish();
-    
-    foreach( $this->get_record_list() as $record )
-    {
-      $this->add_row( $record->id,
-        array( 'consent_form_id' => $record->consent_form_id,
-               'date' => $record->get_consent_form()->date ) );
-    }
-
-    $this->finish_setting_rows();
-  }
-
-  /**
-   * Overrides the parent class method to restrict consent_form_entry list based on user's role
-   * 
-   * @author Patrick Emond <emondpd@mcmaster.ca>
-   * @param database\modifier $modifier Modifications to the list.
-   * @return int
-   * @access protected
-   */
-  protected function determine_record_count( $modifier = NULL )
-  {
-    $db_user = lib::create( 'business\session' )->get_user();
-
-    if( is_null( $modifier ) ) $modifier = lib::create( 'database\modifier' );
-    $modifier->where( 'consent_form.invalid', '!=', true );
-    $modifier->where( 'consent_form.consent_id', '=', NULL );
-    $modifier->where( 'user_id', '=', $db_user->id );
-    $modifier->where( 'deferred', '=', true );
-
-    return parent::determine_record_count( $modifier );
-  }
-  
-  /**
-   * Overrides the parent class method to restrict consent_form_entry list based on user's role
-   * 
-   * @author Patrick Emond <emondpd@mcmaster.ca>
-   * @param database\modifier $modifier Modifications to the list.
-   * @return array( record )
-   * @access protected
-   */
-  protected function determine_record_list( $modifier = NULL )
-  {
-    $db_user = lib::create( 'business\session' )->get_user();
-
-    if( is_null( $modifier ) ) $modifier = lib::create( 'database\modifier' );
-    $modifier->where( 'consent_form.invalid', '!=', true );
-    $modifier->where( 'consent_form.consent_id', '=', NULL );
-    $modifier->where( 'user_id', '=', $db_user->id );
-    $modifier->where( 'deferred', '=', true );
-
-    return parent::determine_record_list( $modifier );
+    parent::__construct( 'consent', $args );
   }
 }
 ?>
