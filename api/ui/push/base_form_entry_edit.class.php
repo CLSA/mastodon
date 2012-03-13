7e8df1efe56267f4e94ddb72741757f4f1f8dbce
Patrick Emond <emondpd@mcmaster.ca>
2012-03-13 14:58:42 -0400

    0000315: Form submitted and was imported into system without typist #2 input

--- api/ui/push/base_form_entry_edit.class.php
+++ api/ui/push/base_form_entry_edit.class.php
@@ -44,6 +44,7 @@ abstract class base_form_entry_edit extends \cenozo\ui\push\base_edit
     {
       $validate_class_name = sprintf( 'ui\pull\%s_form_entry_validate', $this->form_type );
       $get_form_method_name = sprintf( 'get_%s_form', $this->form_type );
+      $entry_list_method_name = sprintf( 'get_%s_form_entry_list', $this->form_type );
 
       $op_validate = lib::create( $validate_class_name, array( 'id' => $this->get_record()->id ) );
       $errors = $op_validate->finish();
@@ -55,7 +56,31 @@ abstract class base_form_entry_edit extends \cenozo\ui\push\base_edit
           __METHOD__ );
 
       parent::finish();
-      $this->get_record()->$get_form_method_name()->import( $this->get_record() );
+
+      // now import the record if the other entry exists, is not deferred and matches this one
+      $db_form = $this->get_record()->$get_form_method_name();
+      $form_entry_mod = lib::create( 'database\modifier' );
+      $form_entry_mod->where( 'deferred', '=', false );
+      $form_entry_mod->where( 'id', '!=', $this->get_record()->id );
+      $form_entry_list =
+        $this->get_record()->$entry_list_method_name( $form_entry_mod );
+      if( 1 == count( $form_entry_list ) )
+      {
+        $match = true;
+        $db_form_entry = current( $form_entry_list );
+        foreach( $db_form_entry->get_column_names() as $column )
+        {
+          if( 'id' != $column &&
+              'user_id' != $column &&
+              $db_form_entry->$column != $this->get_record()->$column )
+          {
+            $match = false;
+            break;
+          }
+        }
+
+        if( $match ) $db_form->import( $this->get_record() );
+      }
     }
     else parent::finish();
   }
