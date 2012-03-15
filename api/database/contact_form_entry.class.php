36bb5e75991992695be7f91f9f80784bdc4605ff
Patrick Emond <emondpd@mcmaster.ca>
2012-03-15 15:33:28 -0400

    Adding space to postal codes

--- api/database/contact_form_entry.class.php
+++ api/database/contact_form_entry.class.php
@@ -15,5 +15,23 @@ use cenozo\lib, cenozo\log, mastodon\util;
  *
  * @package mastodon\database
  */
-class contact_form_entry extends \cenozo\database\record {}
+class contact_form_entry extends \cenozo\database\record
+{
+  /**
+   * Override parent method to make sure 6-character postal codes get a space
+   * 
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @param string $column_name The name of the column
+   * @param mixed $value The value to set the contents of a column to
+   * @throws exception\argument
+   * @access public
+   */
+  public function __set( $column_name, $value )
+  {
+    if( 'postcode' == $column_name && is_string( $value ) && 6 == strlen( $value ) )
+      $value = sprintf( '%s %s', substr( $value, 0, 3 ), substr( $value, 3, 3 ) );
+
+    parent::__set( $column_name, $value );
+  }
+}
 ?>
--- api/database/proxy_form_entry.class.php
+++ api/database/proxy_form_entry.class.php
@@ -15,5 +15,24 @@ use cenozo\lib, cenozo\log, mastodon\util;
  *
  * @package mastodon\database
  */
-class proxy_form_entry extends \cenozo\database\record {}
+class proxy_form_entry extends \cenozo\database\record
+{
+  /**
+   * Override parent method to make sure 6-character postal codes get a space
+   * 
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @param string $column_name The name of the column
+   * @param mixed $value The value to set the contents of a column to
+   * @throws exception\argument
+   * @access public
+   */
+  public function __set( $column_name, $value )
+  {
+    if( ( 'proxy_postcode' == $column_name || 'informant_postcode' == $column_name ) &&
+        is_string( $value ) && 6 == strlen( $value ) )
+      $value = sprintf( '%s %s', substr( $value, 0, 3 ), substr( $value, 3, 3 ) );
+
+    parent::__set( $column_name, $value );
+  }
+}
 ?>
