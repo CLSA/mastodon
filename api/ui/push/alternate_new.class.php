50f8c789691c752eff5eee830a05adb2354d6d55
Patrick Emond <emondpd@mcmaster.ca>
2012-04-12 16:24:50 -0400

    Updating alternate_new push operation
    - can now use participant UID instead of ID
    - allows for missing information (name, association)
    - replaces existing alternates with the same name
    - accepts proxy forms and links to them

--- api/ui/push/alternate_new.class.php
+++ api/ui/push/alternate_new.class.php
@@ -26,6 +26,23 @@ class alternate_new extends \cenozo\ui\push\base_new
    */
   public function __construct( $args )
   {
+    if( array_key_exists( 'noid', $args ) )
+    {
+      // use the noid argument and remove it from the args input
+      $noid = $args['noid'];
+      unset( $args['noid'] );
+
+      // make sure there is sufficient information
+      if( !is_array( $noid ) ||
+          !array_key_exists( 'participant.uid', $noid ) )
+        throw lib::create( 'exception\argument', 'noid', $noid, __METHOD__ );
+      
+      $class_name = lib::get_class_name( 'database\participant' );
+      $db_participant = $class_name::get_unique_record( 'uid', $noid['participant.uid'] );
+      if( !$db_participant ) throw lib::create( 'exception\argument', 'noid', $noid, __METHOD__ );
+      $args['columns']['participant_id'] = $db_participant->id;
+    }
+
     parent::__construct( 'alternate', $args );
   }
 
@@ -38,24 +55,35 @@ class alternate_new extends \cenozo\ui\push\base_new
    */
   public function finish()
   {
+    $alternate_class_name = lib::get_class_name( 'database\alternate' );
+
     // make sure the name and association columns aren't blank
     $columns = $this->get_argument( 'columns' );
     if( !array_key_exists( 'first_name', $columns ) || 0 == strlen( $columns['first_name'] ) )
-      throw lib::create( 'exception\notice', 'The alternate\'s first name cannot be left blank.', __METHOD__ );
+      $columns['first_name'] = 'unknown';
     if( !array_key_exists( 'last_name', $columns ) || 0 == strlen( $columns['last_name'] ) )
-      throw lib::create( 'exception\notice', 'The alternate\'s last name cannot be left blank.', __METHOD__ );
+      $columns['last_name'] = 'unknown';
     if( !array_key_exists( 'association', $columns ) || 0 == strlen( $columns['association'] ) )
-      throw lib::create( 'exception\notice', 'The alternate\'s association cannot be left blank.', __METHOD__ );
+      $columns['association'] = 'unknown';
     
-    foreach( $columns as $column => $value ) $this->get_record()->$column = $value;
+    // special case: replace alternate's with the same first/last name
+    $alternate_mod = lib::create( 'database\modifier' );
+    $alternate_mod->where( 'participant_id', '=', $columns['participant_id'] );
+    $alternate_mod->where( 'first_name', '=', $columns['first_name'] );
+    $alternate_mod->where( 'last_name', '=', $columns['last_name'] );
+    $alternate_list = $alternate_class_name::select( $alternate_mod );
+    if( 0 < count( $alternate_list ) ) $this->set_record( current( $alternate_list ) );
+
+    $db_alternate = $this->get_record();
+    foreach( $columns as $column => $value ) $db_alternate->$column = $value;
 
     try
     {
       // create a person record and like the new record to it
       $db_person = lib::create( 'database\person' );
       $db_person->save();
-      $this->get_record()->person_id = $db_person->id;
-      $this->get_record()->save();
+      $db_alternate->person_id = $db_person->id;
+      $db_alternate->save();
     }
     catch( \cenozo\exception\base_exception $e )
     {
@@ -94,6 +122,24 @@ class alternate_new extends \cenozo\ui\push\base_new
 
         throw $e;
       }
+    }
+
+    // if a form variable was included try to decode it and store it as a proxy form
+    $form = $this->get_argument( 'form', NULL );
+    if( !is_null( $form ) && ( $db_alternate->proxy || $db_alternate->informant ) )
+    {
+      $form_decoded = base64_decode( chunk_split( $form ) );
+      if( false == $form_decoded )
+        throw lib::create( 'exception\runtime', 'Unable to decode form argument.', __METHOD__ );
+
+      // create a new proxy form
+      $db_proxy_form = lib::create( 'database\proxy_form' );
+      if( $db_alternate->proxy ) $db_proxy_form->proxy_alternate_id = $db_alternate->id;
+      if( $db_alternate->informant ) $db_proxy_form->informant_alternate_id = $db_alternate->id;
+      $db_proxy_form->date = util::get_datetime_object()->format( 'Y-m-d' );
+      $db_proxy_form->scan = $form_decoded;
+      $db_proxy_form->complete = true;
+      $db_proxy_form->save();
     }
   }
 }
