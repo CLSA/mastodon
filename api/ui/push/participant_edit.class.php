566f2df6d649a730db6724549f1b3cf9abf94bb0
Patrick Emond <emondpd@mcmaster.ca>
2012-04-18 13:34:48 -0400

    Preferred site can now be updated by Sabretooth/Beartooth

--- api/ui/push/participant_edit.class.php
+++ api/ui/push/participant_edit.class.php
@@ -28,6 +28,9 @@ class participant_edit extends \cenozo\ui\push\base_edit
   {
     if( array_key_exists( 'noid', $args ) )
     {
+      $participant_class_name = lib::get_class_name( 'database\participant' );
+      $site_class_name = lib::get_class_name( 'database\site' );
+
       // use the noid argument and remove it from the args input
       $noid = $args['noid'];
       unset( $args['noid'] );
@@ -37,10 +40,19 @@ class participant_edit extends \cenozo\ui\push\base_edit
           !array_key_exists( 'participant.uid', $noid ) )
         throw lib::create( 'exception\argument', 'noid', $noid, __METHOD__ );
       
-      $class_name = lib::get_class_name( 'database\participant' );
-      $db_participant = $class_name::get_unique_record( 'uid', $noid['participant.uid'] );
+      $db_participant = $participant_class_name::get_unique_record( 'uid', $noid['participant.uid'] );
       if( !$db_participant ) throw lib::create( 'exception\argument', 'noid', $noid, __METHOD__ );
       $args['id'] = $db_participant->id;
+
+      if( is_array( $noid ) &&
+          array_key_exists( 'site.name', $noid ) &&
+          array_key_exists( 'site.cohort', $noid ) )
+      {
+        $db_site = $site_class_name::get_unique_record(
+          array( 'name', 'cohort' ),
+          array( $noid['site.name'], $noid['site.cohort'] ) );
+        $args['columns']['site_id'] = $db_site->id;
+      }
     }
 
     parent::__construct( 'participant', $args );
