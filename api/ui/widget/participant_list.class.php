04537d8eca3f0f3558fd4eb1f8ac41a56957d99f
Patrick Emond <emondpd@mcmaster.ca>
2012-05-30 17:11:24 -0400

    Adding site column to participant list

--- api/ui/widget/participant_list.class.php
+++ api/ui/widget/participant_list.class.php
@@ -47,6 +47,7 @@ class participant_list extends site_restricted_list
     $this->add_column( 'active', 'boolean', 'Active', true );
     $this->add_column( 'source.name', 'string', 'Source', true );
     $this->add_column( 'cohort', 'string', 'Cohort', true );
+    $this->add_column( 'primary_site', 'string', 'Site', false );
 
     // participants are either jurisdiction or participant_site based
     $this->extended_site_selection = true;
@@ -73,6 +74,7 @@ class participant_list extends site_restricted_list
                'active' => $record->active,
                'source.name' => $source_name,
                'cohort' => $record->cohort,
+               'primary_site' => $record->get_primary_site()->name,
                // note count isn't a column, it's used for the note button
                'note_count' => $record->get_note_count() ) );
     }
