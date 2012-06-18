92f163fac6583ba2eddc6e28bd0580cb2eecd8df
Patrick Emond <emondpd@mcmaster.ca>
2012-06-18 17:07:09 -0400

    Adding noncompliant to participant.status enum

--- sql/upgrade/1.1.3/participant.sql
+++ sql/upgrade/1.1.3/participant.sql
@@ -52,3 +52,6 @@ DELIMITER ;
 -- now call the procedure and remove the procedure
 CALL patch_participant();
 DROP PROCEDURE IF EXISTS patch_participant;
+
+-- add the status types to the status column
+ALTER TABLE participant MODIFY status ENUM('deceased','deaf','mentally unfit','language barrier','age range','not canadian','federal reserve','armed forces','institutionalized','noncompliant','other') NULL DEFAULT NULL;
