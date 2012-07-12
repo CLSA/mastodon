0e9c6ccadf73c09257de029e192cfdfe4fc5bfca
Patrick Emond <emondpd@mcmaster.ca>
2012-07-12 14:54:43 -0400

    Removing unnecessary methods

--- api/ui/push/address_delete.class.php
+++ api/ui/push/address_delete.class.php
@@ -46,50 +46,5 @@ class address_delete extends \cenozo\ui\push\base_delete
          ? ( 'comprehensive' == $db_participant->cohort ? BEARTOOTH_URL : SABRETOOTH_URL )
          : NULL );
   }
-
-  /**
-   * Override the parent method to replace the person key with a participant key.
-   * @author Patrick Emond <emondpd@mcmaster.ca>
-   * @param array $args An argument list, usually those passed to the push operation.
-   * @return array
-   * @access protected
-   */
-  protected function convert_to_noid( $args )
-  {
-    $args = parent::convert_to_noid( $args );
-
-    // replace person key with participant key
-    $person_id = $args['noid']['address']['person_id'];
-    unset( $args['noid']['address']['person_id'] );
-    $participant_class_name = lib::get_class_name( 'database\participant' );
-    $db_participant = $this->get_record()->get_person()->get_participant();
-    $args['noid']['address']['participant_id'] =
-      $participant_class_name::get_unique_from_primary_key( $db_participant->id );
-
-    return $args;
-  }
-
-  /**
-   * Override the parent method to replace the participant key with a person key.
-   * @author Patrick Emond <emondpd@mcmaster.ca>
-   * @param array $args An argument list, usually those passed to the push operation.
-   * @return array
-   * @access protected
-   */
-  protected function convert_from_noid( $args )
-  {
-    if( array_key_exists( 'noid', $args ) ) 
-    {   
-      // replace the participant unique key with a person primary key
-      $participant_class_name = lib::get_class_name( 'database\participant' );
-      $participant_id = $participant_class_name::get_primary_from_unique_key(
-        $args['noid']['address']['participant_id'] );
-      unset( $args['noid']['address']['participant_id'] );
-      $db_participant = lib::create( 'database\participant', $participant_id );
-      $args['noid']['address']['person_id'] = $db_participant->person_id;
-    }   
-
-    return parent::convert_from_noid( $args );
-  }
 }
 ?>
