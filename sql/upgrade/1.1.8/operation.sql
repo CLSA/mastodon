715ce132f4ab51db2e349aa575f548380ffd7811
Patrick Emond <emondpd@mcmaster.ca>
2013-01-03 16:47:11 -0500

    Adding charts as an operation type
    - charts are included in their own category in the menu
    - added new "activity" chart which displays server usage measured in processing time

--- sql/upgrade/1.1.8/operation.sql
+++ sql/upgrade/1.1.8/operation.sql
@@ -2,3 +2,5 @@ INSERT IGNORE INTO operation( type, subject, name, restricted, description )
 VALUES( "widget", "withdraw", "report", true, "Set up a withdraw report." );
 INSERT IGNORE INTO operation( type, subject, name, restricted, description )
 VALUES( "pull", "withdraw", "report", true, "Download a withdraw report." );
+INSERT IGNORE INTO operation( type, subject, name, restricted, description )
+VALUES( "widget", "activity", "chart", true, "Displays a chart describing system activity." );
