5bbac8e6ba01a2221db92c79d200ec45dce1e7ff
Patrick Emond <emondpd@mcmaster.ca>
2013-04-19 11:10:35 -0400

    Upgrading mailout report to work with new database design

--- api/ui/widget/mailout_report.class.php
+++ api/ui/widget/mailout_report.class.php
@@ -40,16 +40,23 @@ class mailout_report extends \cenozo\ui\widget\base_report
 
     $this->add_parameter( 'mailed_to', 'boolean', 'Mailed To' );
     $this->add_restriction( 'cohort' );
+    $this->add_restriction( 'service' );
+    $this->add_parameter( 'released', 'enum', 'Released' );
     $this->add_restriction( 'source' );
     $this->add_parameter( 'mark_mailout', 'boolean', 'Mark mailouts as complete' );
     
     $this->set_variable( 'description',
       'This report provides a list of participants with respect to whether they have had a '.
-      'package mailed out to them or not.  When used in the "mailed to = no" mode the list will '.
-      'include all participants who have never been mailed to (whether they have been synched or '.
-      'not).  When used in the "mailed to = yes" mode the list will include all unsynched '.
-      'participants who have been mailed to.  Checking the "mark mailouts" option will mark all '.
-      'participants included in the report as having been mailed out on today\'s date.' );
+      'package mailed out to them or not.  The "mailed to" parameter determines whether to '.
+      'include participants who have ever been mailed to or not.  The "cohort" parameter, if set '.
+      'to anything other than "all", restricts the list to a particular cohort.  The "service" '.
+      'parameter, if set to anything other than "all", restricts the list to participants who '.
+      'belong to a particular service.  The "released" parameter restricts the list to '.
+      'participants who have been released, not released (or either) to the selected service '.
+      '(this parameter is ignored if service is set to "all").  The "source" parameter, if set to '.
+      'anything other than "all", restricts the list to a particular source.  Finally, checking '.
+      'the "mark mailouts" option will mark all participants included in the report as having '.
+      'been mailed out on today\'s date.' );
   }
 
   /**
@@ -62,7 +69,11 @@ class mailout_report extends \cenozo\ui\widget\base_report
   {
     parent::setup();
 
+    $released_list = array( 'Either', 'Yes', 'No' );
+    $released_list = array_combine( $released_list, $released_list );
+
     $this->set_parameter( 'mailed_to', false, true );
+    $this->set_parameter( 'released', key( $released_list ), true, $released_list );
     $this->set_parameter( 'mark_mailout', false, true );
   }
 }
