9725920ad80448fd35e73daf51fc0491dbae1184
Patrick Emond <emondpd@mcmaster.ca>
2013-04-30 15:56:30 -0400

    More changes related to the new role.all_sites column

--- sql/roles.sql
+++ sql/roles.sql
@@ -4,15 +4,15 @@
 SET AUTOCOMMIT=0;
 
 -- make sure all roles exist
-INSERT IGNORE INTO cenozo.role( name, tier ) VALUES
-( "administrator", 3 ),
-( "coordinator", 2 ),
-( "interviewer", 1 ),
-( "onyx", 1 ),
-( "opal", 1 ),
-( "operator", 1 ),
-( "supervisor", 2 ),
-( "typist", 1 );
+INSERT IGNORE INTO cenozo.role( name, tier, all_sites ) VALUES
+( "administrator", 3, true ),
+( "coordinator", 2, false ),
+( "interviewer", 1, false ),
+( "onyx", 1, false ),
+( "opal", 1, true ),
+( "operator", 1, false ),
+( "supervisor", 2, false ),
+( "typist", 1, true );
 
 -- access
 
