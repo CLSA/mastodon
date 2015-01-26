db751a7e838f867831a0b282bb6cba3be72eaa1a
Patrick Emond <emondpd@mcmaster.ca>
2015-01-26 15:00:29 -0500

    Creating service->application rename database patches

--- sql/upgrade/2.0.0/operation.sql
+++ sql/upgrade/2.0.0/operation.sql
@@ -3,3 +3,9 @@ SELECT "Adding new operations" AS "";
 INSERT IGNORE INTO operation( type, subject, name, restricted, description )
 VALUES( "pull", "self", "semaphore_count", false,
 "Provides the total number of active and pending semaphores." );
+
+SELECT "Renaming service operations to application" AS "";
+
+UPDATE operation
+SET subject = "application"
+WHERE subject = "service";
