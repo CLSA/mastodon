f716873985571a75c6a9c16c4cc695cec11b51ed
Patrick Emond <emondpd@mcmaster.ca>
2016-05-04 15:49:19 -0400

    Implementing availability-types

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'mastodon';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.0.0';
-$SETTINGS['general']['build'] = '5375e39';
+$SETTINGS['general']['build'] = 'f7d1210';
 
 // always leave as false when running as production server
 $SETTINGS['general']['development_mode'] = false;
