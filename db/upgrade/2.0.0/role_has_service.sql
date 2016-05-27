608695a932b64c2f900bdfffd8224276b103c64f
Patrick Emond <emondpd@mcmaster.ca>
2016-05-27 16:55:48 -0400

    Adding new report system [WIP]

--- db/upgrade/2.0.0/role_has_service.sql
+++ db/upgrade/2.0.0/role_has_service.sql
@@ -69,9 +69,13 @@ CREATE PROCEDURE patch_role_has_service()
       "FROM ", @cenozo, ".role, service ",
       "WHERE role.name = 'curator' ",
       "AND service.restricted = 1 ",
-      "AND service.subject IN ( ",
-        "'address', 'alternate', 'consent', 'consent_form', 'consent_form_entry', 'event', 'language', ",
-        "'note', 'participant', 'phone', 'proxy_form', 'proxy_form_entry', 'region_site', 'source', 'state' ",
+      "AND ( ",
+        "service.subject IN ( ",
+          "'address', 'alternate', 'consent', 'consent_form', 'consent_form_entry', 'event', 'jurisdiction', ",
+          "'language', 'note', 'participant', 'phone', 'proxy_form', 'proxy_form_entry', 'region_site', ",
+          "'report', 'report_type', 'source', 'state' ",
+        ") ",
+        "OR ( subject = 'report_restriction' AND method = 'GET' ) ",
       ")" );
     PREPARE statement FROM @sql;
     EXECUTE statement;
