c9aba50bc65e8712813babd5729a335c88f624fc
Patrick Emond <emondpd@mcmaster.ca>
2016-09-22 13:51:12 -0400

    Removing need for application_has_cohort in Mastodon.

---
+++ src/service/cohort/module.class.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * module.class.php
+ * 
+ * @author Patrick Emond <emondpd@mcmaster.ca>
+ * @filesource
+ */
+
+namespace mastodon\service\cohort;
+use cenozo\lib, cenozo\log, mastodon\util;
+
+/**
+ * Performs operations which effect how this module is used in a service
+ */
+class module extends \cenozo\service\cohort\module
+{
+  /**
+   * Extend parent method
+   */
+  public function prepare_read( $select, $modifier )
+  {
+    parent::prepare_read( $select, $modifier );
+
+    // remove the requirement for rows in application_has_cohort (mastodon gets all cohorts)
+    $modifier->remove_where( 'application_has_cohort.application_id' );
+    $modifier->remove_join( 'application_has_cohort' );
+  }
+}
