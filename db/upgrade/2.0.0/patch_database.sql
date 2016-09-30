4823679cb331341eef21b6488d6966e9d75cb0aa
Patrick Emond <emondpd@mcmaster.ca>
2016-09-30 13:47:00 -0400

    Creating export database tables
    - removed participant export utility
    - created export, export_column and export_restriction modules

--- db/upgrade/2.0.0/patch_database.sql
+++ db/upgrade/2.0.0/patch_database.sql
@@ -38,6 +38,9 @@ SOURCE report_type.sql
 SOURCE application_type_has_report_type.sql
 SOURCE role_has_report_type.sql
 SOURCE application_has_script.sql
+SOURCE export.sql
+SOURCE export_column.sql
+SOURCE export_restriction.sql
 
 SOURCE table_character_sets.sql
 SOURCE column_character_sets.sql
