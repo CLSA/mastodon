8e7591813573bee6bf08ebf5c12c47af7d8a956b
Patrick Emond <emondpd@mcmaster.ca>
2016-11-17 16:16:14 -0500

    Fixing a bug when using multiedit in mastodon

---
+++ src/database/participant.class.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * participant.class.php
+ * 
+ * @author Patrick Emond <emondpd@mcmaster.ca>
+ * @filesource
+ */
+
+namespace mastodon\database;
+use cenozo\lib, cenozo\log, mastodon\util;
+
+/**
+ * participant: record
+ */
+class participant extends \cenozo\database\participant
+{
+  /**
+   * Replace parent method
+   */
+  public static function get_valid_uid_list( $uid_list, $db_application = NULL, $unreleased = false )
+  {
+    $output_uid_list = array();
+    
+    if( !is_array( $uid_list ) ) 
+    {   
+      // sanitize the entries
+      $uid_list = explode( ' ', // delimite string by spaces and create array from result
+                  preg_replace( '/[^a-zA-Z0-9 ]/', '', // remove anything that isn't a letter, number of space
+                  preg_replace( '/[\s,;|\/]/', ' ', // replace whitespace and separation chars with a space
+                  strtoupper( $uid_list ) ) ) ); // convert to uppercase
+    }   
+
+    // match UIDs (eg: A123456)
+    $uid_list = array_filter( $uid_list, function( $string ) { 
+      return 1 == preg_match( '/^[A-Z][0-9]{6}$/', $string );
+    } );
+
+    if( 0 < count( $uid_list ) )
+    {
+      $session = lib::create( 'business\session' );
+      $db_site = $session->get_site();
+      $db_role = $session->get_role();
+
+      // make list unique and sort it
+      $uid_list = array_unique( $uid_list );
+      sort( $uid_list );
+
+      // go through the list and remove invalid UIDs
+      $select = lib::create( 'database\select' );
+      $select->add_column( 'uid' );
+      $select->from( 'participant' );
+      $modifier = lib::create( 'database\modifier' );
+      $modifier->where( 'uid', 'IN', $uid_list );
+      $modifier->order( 'uid' );
+      
+      if( !is_null( $db_application ) )
+      {
+        // restrict to participant cohorts in the given application
+        $modifier->join( 'application_has_cohort', 'participant.cohort_id', 'application_has_cohort.cohort_id' );
+        $modifier->join( 'application', 'application_has_cohort.application_id', 'application.id' );
+        $modifier->where( 'application.id', '=', $db_application->id );
+
+        // restrict to released or unreleased participants
+        if( $unreleased )
+        {
+          $sub_mod = lib::create( 'database\modifier' );
+          $sub_mod->where( 'participant.id', '=', 'application_has_participant.participant_id', false );
+          $sub_mod->where( 'application_has_participant.application_id', '=', 'application.id', false );
+          $modifier->join_modifier( 'application_has_participant', $sub_mod, 'left' );
+          $modifier->where( 'application_has_participant.datetime', '=', NULL );
+        }
+      }
+
+      foreach( static::select( $select, $modifier ) as $row ) $output_uid_list[] = $row['uid'];
+    }
+
+    return $output_uid_list;
+  }
+}
--- src/service/participant/post.class.php
+++ src/service/participant/post.class.php
@@ -12,16 +12,8 @@ use cenozo\lib, cenozo\log, mastodon\util;
 /**
  * The base class of all post services.
  */
-class post extends \cenozo\service\service
+class post extends \cenozo\service\participant\post
 {
-  /**
-   * Extends parent constructor
-   */
-  public function __construct( $path, $args, $file )
-  {
-    parent::__construct( 'POST', $path, $args, $file );
-  }
-
   /**
    * Extends parent method
    */
@@ -46,10 +38,6 @@ class post extends \cenozo\service\service
           catch( \cenozo\exception\runtime $e ) { $this->status->set_code( 404 ); }
         }
       }
-      else
-      {
-        $this->db_application = $session->get_application();
-      }
 
       if( array_key_exists( 'site_id', $file ) )
       {
@@ -66,7 +54,7 @@ class post extends \cenozo\service\service
 
             $site_mod = lib::create( 'database\modifier' );
             $site_mod->where( 'site.id', '=', $this->db_site->id );
-            if( 0 == $this->db_application->get_site_count( $site_mod ) )
+            if( is_null( $this->db_application ) || 0 == $this->db_application->get_site_count( $site_mod ) )
               $this->status->set_code( 400 );
           }
         }
@@ -90,127 +78,78 @@ class post extends \cenozo\service\service
    */
   protected function execute()
   {
-    parent::execute();
-
-    $util_class_name = lib::get_class_name( 'util' );
-    $participant_class_name = lib::get_class_name( 'database\participant' );
-    $session = lib::create( 'business\session' );
-    $db_role = $session->get_role();
-
-    // This is a special service since participants cannot be added to the system through the web interface.
-    // Instead, this service provides participant-based utility functions.
     $file = $this->get_file_as_array();
-    if( array_key_exists( 'uid_list', $file ) && is_array( $file['uid_list'] ) )
+    $mode = array_key_exists( 'mode', $file ) ? $file['mode'] : NULL;
+    if( is_null( $mode ) ) parent::execute();
+    else
     {
-      $mode = array_key_exists( 'mode', $file ) ? $file['mode'] : NULL;
+      $participant_class_name = lib::get_class_name( 'database\participant' );
 
-      $uid_list = array();
-      
-      if( 0 < count( $file['uid_list'] ) )
+      // This is a special service since participants cannot be added to the system through the web interface.
+      // Instead, this service provides participant-based utility functions.
+      if( array_key_exists( 'uid_list', $file ) )
       {
-        // go through the list and remove invalid UIDs
-        $select = lib::create( 'database\select' );
-        $select->add_column( 'uid' );
-        $select->from( 'participant' );
-        $modifier = lib::create( 'database\modifier' );
-        $modifier->where( 'uid', 'IN', $file['uid_list'] );
-        $modifier->order( 'uid' );
-        
-        if( !$db_role->all_sites || !is_null( $mode ) )
-        {
-          // restrict to participant cohorts in the given application
-          $modifier->join( 'application_has_cohort', 'participant.cohort_id', 'application_has_cohort.cohort_id' );
-          $modifier->join( 'application', 'application_has_cohort.application_id', 'application.id' );
-          $modifier->where( 'application.id', '=', $this->db_application->id );
-        }
+        $uid_list = $participant_class_name::get_valid_uid_list(
+          $file['uid_list'],
+          $this->db_application,
+          'unreleased_only' == $mode || 'release' == $mode
+        );
 
-        // restrict by site
-        if( !$db_role->all_sites )
-        {
-          $sub_mod = lib::create( 'database\modifier' );
-          $sub_mod->where( 'participant.id', '=', 'participant_site.participant_id', false );
-          $sub_mod->where( 'participant_site.application_id', '=', 'application.id', false );
-          $sub_mod->where( 'participant_site.site_id', '=', $session->get_site()->id );
-          $modifier->join_modifier( 'participant_site', $sub_mod );
-        }
-
-        // restrict if in mode
-        if( !is_null( $mode ) )
-        {
-          $sub_mod = lib::create( 'database\modifier' );
-          $sub_mod->where( 'participant.id', '=', 'application_has_participant.participant_id', false );
-          $sub_mod->where( 'application_has_participant.application_id', '=', 'application.id', false );
-
-          if( 'released_only' == $mode )
-            $sub_mod->where( 'application_has_participant.datetime', '!=', NULL );
-          else // unreleased_only or release
-            $modifier->where( 'application_has_participant.datetime', '=', NULL );
-
-          $modifier->join_modifier(
-            'application_has_participant', $sub_mod, 'released_only' == $mode ? '' : 'left' );
-        }
-
-        foreach( $participant_class_name::select( $select, $modifier ) as $row ) $uid_list[] = $row['uid'];
-      }
-
-      if( 'release' == $mode )
-      { // release the participants
-        if( 0 < count( $uid_list ) )
-        {
-          $modifier = lib::create( 'database\modifier' );
-          $modifier->where( 'participant.uid', 'IN', $uid_list );
-          $this->db_application->release_participants( $modifier );
-        }
-      }
-      else if( 'preferred_site' == $mode )
-      { // change the participants' preferred site
-        if( 0 < count( $uid_list ) )
-        {
-          $modifier = lib::create( 'database\modifier' );
-          $modifier->where( 'participant.uid', 'IN', $uid_list );
-          $this->db_application->set_preferred_site( $modifier, $this->db_site );
-        }
-      }
-      else if( !is_null( $mode ) ) // any other release mode
-      { // return a list of all valid uids and count by cohort and site
-        $site_list = array();
-
-        if( 0 < count( $uid_list ) )
-        {
-          $site_sel = lib::create( 'database\select' );
-          $site_sel->from( 'participant' );
-          $site_sel->add_table_column( 'cohort', 'name', 'cohort' );
-          $site_sel->add_table_column( 'site', 'name', 'site' );
-          $site_sel->add_column( 'COUNT(*)', 'total', false );
-          $site_mod = lib::create( 'database\modifier' );
-          $site_mod->join( 'cohort', 'participant.cohort_id', 'cohort.id' );
-          $join_mod = lib::create( 'database\modifier' );
-          $join_mod->where( 'participant.id', '=', 'participant_site.participant_id', false );
-          $join_mod->where( 'participant_site.application_id', '=', $this->db_application->id );
-          $site_mod->join_modifier( 'participant_site', $join_mod );
-          $site_mod->join( 'site', 'participant_site.site_id', 'site.id' );
-          $site_mod->where( 'participant.uid', 'IN', $uid_list );
-          $site_mod->group( 'cohort.id' );
-          $site_mod->group( 'site.id' );
-
-          foreach( $participant_class_name::select( $site_sel, $site_mod ) as $row )
+        if( 'release' == $mode )
+        { // release the participants
+          if( 0 < count( $uid_list ) )
           {
-            if( !array_key_exists( $row['cohort'], $site_list ) ) $site_list[$row['cohort']] = array();
-            $site_list[$row['cohort']][$row['site']] = $row['total'];
+            $modifier = lib::create( 'database\modifier' );
+            $modifier->where( 'participant.uid', 'IN', $uid_list );
+            $this->db_application->release_participants( $modifier );
           }
         }
+        else if( 'preferred_site' == $mode )
+        { // change the participants' preferred site
+          if( 0 < count( $uid_list ) )
+          {
+            $modifier = lib::create( 'database\modifier' );
+            $modifier->where( 'participant.uid', 'IN', $uid_list );
+            $this->db_application->set_preferred_site( $modifier, $this->db_site );
+          }
+        }
+        else // any other mode
+        { // return a list of all valid uids and count by cohort and site
+          $site_list = array();
 
-        $this->set_data( array(
-          'uid_list' => $uid_list,
-          'site_list' => $site_list
-        ) );
+          if( 0 < count( $uid_list ) )
+          {
+            $site_sel = lib::create( 'database\select' );
+            $site_sel->from( 'participant' );
+            $site_sel->add_table_column( 'cohort', 'name', 'cohort' );
+            $site_sel->add_table_column( 'site', 'name', 'site' );
+            $site_sel->add_column( 'COUNT(*)', 'total', false );
+            $site_mod = lib::create( 'database\modifier' );
+            $site_mod->join( 'cohort', 'participant.cohort_id', 'cohort.id' );
+            $join_mod = lib::create( 'database\modifier' );
+            $join_mod->where( 'participant.id', '=', 'participant_site.participant_id', false );
+            $join_mod->where( 'participant_site.application_id', '=', $this->db_application->id );
+            $site_mod->join_modifier( 'participant_site', $join_mod );
+            $site_mod->join( 'site', 'participant_site.site_id', 'site.id' );
+            $site_mod->where( 'participant.uid', 'IN', $uid_list );
+            $site_mod->group( 'cohort.id' );
+            $site_mod->group( 'site.id' );
+
+            foreach( $participant_class_name::select( $site_sel, $site_mod ) as $row )
+            {
+              if( !array_key_exists( $row['cohort'], $site_list ) ) $site_list[$row['cohort']] = array();
+              $site_list[$row['cohort']][$row['site']] = $row['total'];
+            }
+          }
+
+          $this->set_data( array(
+            'uid_list' => $uid_list,
+            'site_list' => $site_list
+          ) );
+        }
       }
-      else // return a list of all valid uids
-      {
-        $this->set_data( $uid_list );
-      }
+      else $this->status->set_code( 400 ); // must provide a uid_list
     }
-    else $this->status->set_code( 400 ); // must provide a uid_list
   }
 
   /**
