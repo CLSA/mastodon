db28763db9eafb82ba2b532068c1d7afe242da36
Patrick Emond <emondpd@mcmaster.ca>
2017-04-12 11:51:41 -0400

    Fixing missing uib- to angular-ui components

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'mastodon';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.1.0';
-$SETTINGS['general']['build'] = '93b2ab5';
+$SETTINGS['general']['build'] = 'ee0dfb9';
 
 // always leave as false when running as production server
 $SETTINGS['general']['development_mode'] = false;
