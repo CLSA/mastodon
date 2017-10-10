448049d94a1ecef5ea51d1ebd5e9e776b6fc1e7a
Patrick Emond <emondpd@mcmaster.ca>
2017-10-10 16:11:14 -0400

    Adding opal_form_template services

--- db/upgrade/2.1.0/service.sql
+++ db/upgrade/2.1.0/service.sql
@@ -9,4 +9,12 @@ INSERT IGNORE INTO service ( subject, method, resource, restricted ) VALUES
 ( 'general_proxy_form_entry', 'GET', 0, 1 ),
 ( 'general_proxy_form_entry', 'GET', 1, 1 ),
 ( 'general_proxy_form_entry', 'PATCH', 1, 1 ),
-( 'general_proxy_form_entry', 'POST', 0, 1 );
+( 'general_proxy_form_entry', 'POST', 0, 1 ),
+( 'opal_form_template', 'DELETE', 1, 1 ),
+( 'opal_form_template', 'GET', 0, 1 ),
+( 'opal_form_template', 'GET', 1, 1 ),
+( 'opal_form_template', 'PATCH', 1, 1 ),
+( 'opal_form_template', 'POST', 0, 1 ),
+( 'opal_form_template_file', 'DELETE', 1, 1 ),
+( 'opal_form_template_file', 'GET', 0, 1 ),
+( 'opal_form_template_file', 'POST', 0, 1 );
