0f1ee82f24bb06d7a959f6c9f0626b382f9207aa
Patrick Emond <emondpd@mcmaster.ca>
2017-10-12 16:47:05 -0400

    Fixing bugs in sql upgrade script

--- db/upgrade/2.1.0/service.sql
+++ db/upgrade/2.1.0/service.sql
@@ -10,8 +10,5 @@ INSERT IGNORE INTO service ( subject, method, resource, restricted ) VALUES
 ( 'general_proxy_form_entry', 'GET', 1, 1 ),
 ( 'general_proxy_form_entry', 'PATCH', 1, 1 ),
 ( 'general_proxy_form_entry', 'POST', 0, 1 ),
-( 'opal_form_template', 'DELETE', 1, 1 ),
 ( 'opal_form_template', 'GET', 0, 1 ),
-( 'opal_form_template', 'GET', 1, 1 ),
-( 'opal_form_template', 'PATCH', 1, 1 ),
-( 'opal_form_template', 'POST', 0, 1 );
+( 'opal_form_template', 'GET', 1, 1 );
