bd764ec04c4b92163f3795bb933f07280084e84a
Patrick Emond <emondpd@mcmaster.ca>
2019-01-18 13:38:51 -0500

    Adding new general/uid_regex setting so that UIDs can have any format

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'mastodon';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.3';
-$SETTINGS['general']['build'] = 'c2592b6';
+$SETTINGS['general']['build'] = 'eebb32d';
 
 // the location of mastodon internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
