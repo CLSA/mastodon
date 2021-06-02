2c6ebd543da2edadbdfdcb6781bbcfa5d048a0c7
Patrick Emond <emondpd@mcmaster.ca>
2021-06-02 13:49:23 -0400

    Removing access to consent services from some roles

--- db/upgrade/2.7/service.sql
+++ db/upgrade/2.7/service.sql
@@ -3,6 +3,11 @@ SELECT "Adding new services" AS "";
 DELETE FROM service WHERE subject IN( 'proxy_consent', 'proxy_consent_form' );
 
 INSERT IGNORE INTO service ( subject, method, resource, restricted ) VALUES
+( 'alternate_consent', 'DELETE', 1, 1 ),
+( 'alternate_consent', 'GET', 0, 0 ),
+( 'alternate_consent', 'GET', 1, 0 ),
+( 'alternate_consent', 'PATCH', 1, 1 ),
+( 'alternate_consent', 'POST', 0, 0 ),
 ( 'dm_consent_form', 'GET', 0, 1 ),
 ( 'dm_consent_form', 'GET', 1, 1 ),
 ( 'dm_consent_form', 'PATCH', 1, 1 ),
@@ -18,3 +23,6 @@ INSERT IGNORE INTO service ( subject, method, resource, restricted ) VALUES
 ( 'ip_consent_form_entry', 'PATCH', 1, 1 ),
 ( 'ip_consent_form_entry', 'POST', 0, 1 ),
 ( 'proxy_type', 'PATCH', 1, 1 );
+
+-- restrictions on adding consent records is managed by restricting consent-types by role
+UPDATE service SET restricted = 0 WHERE subject = 'consent' AND method = 'POST';
