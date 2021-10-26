545823b8ca7fb3c83cb3334212aac7fb67849731
Patrick Emond <emondpd@mcmaster.ca>
2021-10-26 15:29:33 -0400

    Bumping to version 2.8

--- settings.ini.php
+++ settings.ini.php
@@ -12,7 +12,7 @@ global $SETTINGS;
 // tagged version
 $SETTINGS['general']['application_name'] = 'mastodon';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
-$SETTINGS['general']['version'] = '2.7';
+$SETTINGS['general']['version'] = '2.8';
 $SETTINGS['general']['build'] = 'f59ef6c';
 
 // the location of mastodon internal path
