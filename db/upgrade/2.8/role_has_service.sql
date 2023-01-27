2dfaaabd729bc0c9a7e5fc0e2789fb6970ce9281
Patrick Emond <emondpd@mcmaster.ca>
2023-01-27 13:07:46 -0500

    Feature: allowing helpline to delete un-sent emails

--- db/upgrade/2.8/role_has_service.sql
+++ db/upgrade/2.8/role_has_service.sql
@@ -62,6 +62,19 @@ CREATE PROCEDURE patch_role_has_service()
     EXECUTE statement;
     DEALLOCATE PREPARE statement;
 
+    SET @sql = CONCAT(
+      "INSERT IGNORE INTO role_has_service( role_id, service_id ) ",
+      "SELECT role.id, service.id ",
+      "FROM ", @cenozo, ".role, service ",
+      "WHERE role.name IN( 'administrator', 'curator', 'helpline' ) ",
+      "AND service.subject = 'mail' ",
+      "AND service.method = 'DELETE' ",
+      "AND service.restricted = 1"
+    );
+    PREPARE statement FROM @sql;
+    EXECUTE statement;
+    DEALLOCATE PREPARE statement;
+
   END //
 DELIMITER ;
 
