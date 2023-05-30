868c8cd2c69fd552efbb42cec4da7d301c421be5
Patrick Emond <emondpd@mcmaster.ca>
2023-05-30 11:33:12 -0400

    Bug: disabled button was clickable

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'mastodon';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.9';
-$SETTINGS['general']['build'] = '44d28f6';
+$SETTINGS['general']['build'] = 'f67a192';
 
 // the location of mastodon internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
