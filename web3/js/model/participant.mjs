f1c8bd1bab64130b6989cf8ce0951ee392ac19ba
Patrick Emond <emondpd@mcmaster.ca>
2025-10-23 11:26:17 -0400

    v3: changing some participant child list titles

--- web3/js/model/participant.mjs
+++ web3/js/model/participant.mjs
@@ -3,6 +3,15 @@ const classes = await import(`${CENOZO_URL}/js/model/participant.mjs`);
 
 const base_view_class = classes.CN_participant_view ? classes.CN_participant_view : CN_base_view;
 export class CN_participant_view extends base_view_class {
-  // TODO: change "Application" sub-list title to "Release"
-  // TODO: change "Study" sub-list title to "Eligible Study"
+  /**
+   * Extend parent method
+   */
+  get_selector_child_list() {
+    return super.get_selector_child_list().map(child => {
+      console.log(child.title);
+      if ("Applications" == child.title) child.title = "Release";
+      if ("Studies" == child.title) child.title = "Eligible Studies";
+      return child;
+    }).sort((a,b) => a.title>b.title);
+  }
 }
