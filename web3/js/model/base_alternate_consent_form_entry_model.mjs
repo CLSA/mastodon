e7db15a48786f4ab5a2d0d58fc5588d6669fd141
Patrick Emond <emondpd@mcmaster.ca>
2026-04-17 14:14:09 -0400

    v3: fixing bugs

--- web3/js/model/base_alternate_consent_form_entry_model.mjs
+++ web3/js/model/base_alternate_consent_form_entry_model.mjs
@@ -23,36 +23,47 @@ export class CN_base_alternate_consent_form_entry_model extends CN_base_form_ent
                 // NOTE: this is a special typeahead that can't be pulled from the alternate model because it
                 // references this model's participant_id property
                 get_list: async (value, form_input) => {
-                  const participant_id = await form_input.get_action().get_formatted_property("participant_id");
-                  return await CN_api.get(`participant/${participant_id}/alternate`, {
-                    select: {
-                      column: [{
-                        table: "alternate",
-                        column: "id",
-                        alias: "key",
-                      }, {
-                        table: "alternate",
-                        column:
-                          'CONCAT( alternate.first_name, " ", alternate.last_name, " (", alternate_type_list, ")" )',
-                        alias: "value",
-                        table_prefix: false,
-                      }],
-                    },
-                    modifier: {
-                      where: [
-                        { bracket: true, open: true },
-                        { column: "alternate_type_list", operator: "LIKE", value: "%Decision Maker%" },
-                        { column: "alternate_type_list", operator: "LIKE", value: "%Information Provider%", or: true },
-                        { bracket: true, open: false },
-                        { bracket: true, open: true },
-                        { column: "alternate.first_name", operator: "like", value: `%${value}%`, or: true },
-                        { column: "alternate.last_name", operator: "like", value: `%${value}%`, or: true },
-                        { bracket: true, open: false },
-                      ],
-                      order: 'CONCAT( alternate.first_name, " ", alternate.last_name )',
-                      limit: 20,
-                    },
-                  });
+                  const participant_id =
+                    await form_input.get_action().get_property_value_for_record("participant_id");
+                  return (
+                    !participant_id ?
+                    [] :
+                    await CN_api.get(`participant/${participant_id}/alternate`, {
+                      select: {
+                        column: [{
+                          table: "alternate",
+                          column: "id",
+                          alias: "key",
+                        }, {
+                          table: "alternate",
+                          column: `CONCAT(
+                            alternate.first_name,
+                            " ",
+                            alternate.last_name,
+                            " (",
+                            alternate_type_list,
+                            ")"
+                          )`,
+                          alias: "value",
+                          table_prefix: false,
+                        }],
+                      },
+                      modifier: {
+                        where: [
+                          { bracket: true, open: true },
+                          { column: "alternate_type_list", operator: "LIKE", value: "%Information Provider%" },
+                          { column: "alternate_type_list", operator: "LIKE", value: "%Decision Maker%", or: true },
+                          { bracket: true, open: false },
+                          { bracket: true, open: true },
+                          { column: "alternate.first_name", operator: "like", value: `%${value}%`, or: true },
+                          { column: "alternate.last_name", operator: "like", value: `%${value}%`, or: true },
+                          { bracket: true, open: false },
+                        ],
+                        order: 'CONCAT( alternate.first_name, " ", alternate.last_name )',
+                        limit: 20,
+                      },
+                    })
+                  );
                 },
               },
             },
