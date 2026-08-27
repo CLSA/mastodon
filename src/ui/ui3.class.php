c129de9641b14010ed2c4e3c14db2aaaca1746a8
Patrick Emond <emondpd@mcmaster.ca>
2026-08-27 16:09:04 -0400

    Refactor: redesigning v3 server-side module/menu build system

--- src/ui/ui3.class.php
+++ src/ui/ui3.class.php
@@ -16,19 +16,18 @@ class ui3 extends \cenozo\ui\ui3
   /**
    * Extends the parent method
    */
-  public static function generate()
+  protected function generate_modules()
   {
-    $data = parent::generate();
+    parent::generate_modules();
 
     $db_role = lib::create( 'business\session' )->get_role();
 
-    // add child actions to certain modules
-    if( 2 <= $db_role->tier && array_key_exists( 'application', $data['module_list'] ) )
-      $data['module_list']['application']->add_action( 'release', '/{identifier}' );
+    $module = $this->get_module( 'application' );
+    if( !is_null( $module ) && 2 <= $db_role->tier ) $module->add_action( 'release', '/{identifier}' );
 
-    if( array_key_exists( 'participant', $data['module_list'] ) )
+    $module = $this->get_module( 'participant' );
+    if( !is_null( $module ) )
     {
-      $module = $data['module_list']['participant'];
       $module->add_child( 'application', 0 );
       if( 2 <= $db_role->tier ) $module->add_action( 'release', '/{identifier}' );
       if( in_array( $db_role->name, ['administrator', 'curator'] ) )
@@ -36,182 +35,82 @@ class ui3 extends \cenozo\ui\ui3
       $module->add_child( 'study_phase_status' );
     }
 
-    if( array_key_exists( 'user', $data['module_list'] ) )
+    $user_module = $this->get_module( 'user' );
+    foreach( $this->form_type_list as $form_type )
     {
-      $module = $data['module_list']['user'];
-      $module->add_child( 'consent_form_entry', 0 );
-      $module->add_child( 'contact_form_entry', 0 );
-      $module->add_child( 'general_proxy_form_entry', 0 );
-      $module->add_child( 'proxy_form_entry', 0 );
-      $module->add_child( 'dm_consent_form_entry', 0 );
-      $module->add_child( 'ip_consent_form_entry', 0 );
+      if( !is_null( $module ) ) $user_module->add_child( sprintf( '%s_form_entry', $form_type ), 0 );
+
+      $module = $this->get_module( sprintf( '%s_form', $form_type ) );
+      if( !is_null( $module ) )
+      {
+        $module->add_child( sprintf( '%s_form_entry', $form_type ) );
+        if( 2 <= $db_role->tier ) $module->add_action( 'adjudicate', '/{identifier}' );
+      }
+
+      $module = $this->get_module( sprintf( '%s_form_entry', $form_type ) );
+      if( !is_null( $module ) ) $module->remove_action( 'add' );
     }
 
-    // consent form
-    if( array_key_exists( 'consent_form', $data['module_list'] ) )
+    $module = $this->get_module( 'participant_data' );
+    if( !is_null( $module ) )
     {
-      $module = $data['module_list']['consent_form'];
-      $module->add_child( 'consent_form_entry' );
-      if( 2 <= $db_role->tier ) $module->add_action( 'adjudicate', '/{identifier}' );
-    }
-    if( array_key_exists( 'consent_form_entry', $data['module_list'] ) )
-    {
-      $module = $data['module_list']['consent_form_entry'];
-      $module->remove_action( 'add' );
-    }
-
-    // contact form
-    if( array_key_exists( 'contact_form', $data['module_list'] ) )
-    {
-      $module = $data['module_list']['contact_form'];
-      $module->add_child( 'contact_form_entry' );
-      if( 2 <= $db_role->tier ) $module->add_action( 'adjudicate', '/{identifier}' );
-    }
-    if( array_key_exists( 'contact_form_entry', $data['module_list'] ) )
-    {
-      $module = $data['module_list']['contact_form_entry'];
-      $module->remove_action( 'add' );
-    }
-
-    // hin form
-    if( array_key_exists( 'hin_form', $data['module_list'] ) )
-    {
-      $module = $data['module_list']['hin_form'];
-      $module->add_child( 'hin_form_entry' );
-      if( 2 <= $db_role->tier ) $module->add_action( 'adjudicate', '/{identifier}' );
-    }
-    if( array_key_exists( 'hin_form_entry', $data['module_list'] ) )
-    {
-      $module = $data['module_list']['hin_form_entry'];
-      $module->remove_action( 'add' );
-    }
-
-    // extended hin form
-    if( array_key_exists( 'extended_hin_form', $data['module_list'] ) )
-    {
-      $module = $data['module_list']['extended_hin_form'];
-      $module->add_child( 'extended_hin_form_entry' );
-      if( 2 <= $db_role->tier ) $module->add_action( 'adjudicate', '/{identifier}' );
-    }
-    if( array_key_exists( 'extended_hin_form_entry', $data['module_list'] ) )
-    {
-      $module = $data['module_list']['extended_hin_form_entry'];
-      $module->remove_action( 'add' );
-    }
-
-    // general proxy form
-    if( array_key_exists( 'general_proxy_form', $data['module_list'] ) )
-    {
-      $module = $data['module_list']['general_proxy_form'];
-      $module->add_child( 'general_proxy_form_entry' );
-      if( 2 <= $db_role->tier ) $module->add_action( 'adjudicate', '/{identifier}' );
-    }
-    if( array_key_exists( 'general_proxy_form_entry', $data['module_list'] ) )
-    {
-      $module = $data['module_list']['general_proxy_form_entry'];
-      $module->remove_action( 'add' );
-    }
-
-    // proxy form
-    if( array_key_exists( 'proxy_form', $data['module_list'] ) )
-    {
-      $module = $data['module_list']['proxy_form'];
-      $module->add_child( 'proxy_form_entry' );
-      if( 2 <= $db_role->tier ) $module->add_action( 'adjudicate', '/{identifier}' );
-    }
-    if( array_key_exists( 'proxy_form_entry', $data['module_list'] ) )
-    {
-      $module = $data['module_list']['proxy_form_entry'];
-      $module->remove_action( 'add' );
-    }
-
-    // decision maker consent form
-    if( array_key_exists( 'dm_consent_form', $data['module_list'] ) )
-    {
-      $module = $data['module_list']['dm_consent_form'];
-      $module->add_child( 'dm_consent_form_entry' );
-      if( 2 <= $db_role->tier ) $module->add_action( 'adjudicate', '/{identifier}' );
-    }
-    if( array_key_exists( 'dm_consent_form_entry', $data['module_list'] ) )
-    {
-      $module = $data['module_list']['dm_consent_form_entry'];
-      $module->remove_action( 'add' );
-    }
-
-    // information provider consent form
-    if( array_key_exists( 'ip_consent_form', $data['module_list'] ) )
-    {
-      $module = $data['module_list']['ip_consent_form'];
-      $module->add_child( 'ip_consent_form_entry' );
-      if( 2 <= $db_role->tier ) $module->add_action( 'adjudicate', '/{identifier}' );
-    }
-    if( array_key_exists( 'ip_consent_form_entry', $data['module_list'] ) )
-    {
-      $module = $data['module_list']['ip_consent_form_entry'];
-      $module->remove_action( 'add' );
-    }
-
-    if( array_key_exists( 'participant_data', $data['module_list'] ) )
-    {
-      $module = $data['module_list']['participant_data'];
       $module->add_choose( 'cohort' );
       $module->add_child( 'participant_data_template' );
     }
 
-    if( array_key_exists( 'study_phase', $data['module_list'] ) )
+    $module = $this->get_module( 'study_phase' );
+    if( !is_null( $module ) )
     {
-      $module = $data['module_list']['study_phase'];
       $module->add_child( 'participant_data' );
       $module->add_child( 'study_phase_status' );
     }
-
-    // remove all parent list and utilities for typists
-    if( 'typist' == $db_role->name )
-    {
-      $data['menu']['lists'] = [];
-      $data['menu']['utilities'] = [];
-    }
-
-    // remove the application list from non admins
-    if( 3 > $db_role->tier ) unset( $data['menu']['lists']['Applications'] );
-
-    // add application-specific states to the base list
-    $menu_list_items = [
-      ['subject' => 'consent_form', 'title' => 'Consent Forms'],
-      ['subject' => 'contact_form', 'title' => 'Contact Forms'],
-      ['subject' => 'hin_form', 'title' => 'HIN Forms'],
-      ['subject' => 'extended_hin_form', 'title' => 'Extended HIN Forms'],
-      ['subject' => 'general_proxy_form', 'title' => 'General Proxy Forms'],
-      ['subject' => 'proxy_form', 'title' => 'Proxy Forms'],
-      ['subject' => 'dm_consent_form', 'title' => 'Proxy DM Forms'],
-      ['subject' => 'ip_consent_form', 'title' => 'Proxy IP Forms'],
-      ['subject' => 'study', 'title' => 'Studies'],
-    ];
-
-    if( 'typist' == $db_role->name )
-    {
-      $menu_list_items = [
-        ['subject' => 'consent_form_entry', 'title' => 'Consent Form Entries'],
-        ['subject' => 'contact_form_entry', 'title' => 'Contact Form Entries'],
-        ['subject' => 'hin_form_entry', 'title' => 'HIN Form Entries'],
-        ['subject' => 'extended_hin_form_entry', 'title' => 'Extended HIN Form Entries'],
-        ['subject' => 'general_proxy_form_entry', 'title' => 'General Proxy Form Entries'],
-        ['subject' => 'proxy_form_entry', 'title' => 'Proxy Form Entries'],
-        ['subject' => 'dm_consent_form_entry', 'title' => 'Proxy DM Form Entries'],
-        ['subject' => 'ip_consent_form_entry', 'title' => 'Proxy IP Form Entries'],
-      ];
-    }
-
-    foreach( $menu_list_items as $item )
-    {
-      if( array_key_exists( $item['subject'], $data['module_list'] ) )
-      {
-        $module = $data['module_list'][$item['subject']];
-        if( $module->get_list_menu() && $module->has_action( 'list' ) )
-          $data['menu']['lists'][$item['title']] = $item['subject'];
-      }
-    }
-
-    return $data;
   }
+
+  /**
+   * Extends the parent method
+   */
+  protected function generate_menus()
+  {
+    parent::generate_menus();
+
+    $db_role = lib::create( 'business\session' )->get_role();
+
+    // remove all parent lists and utilities for typists
+    if( 'typist' == $db_role->name )
+    {
+      $this->remove_all_menu_items( 'list' );
+      $this->remove_all_menu_items( 'utility' );
+    }
+
+    foreach( $this->form_type_list as $form_type )
+    {
+      $title = sprintf(
+        '%s Form',
+        ucWords(
+          str_replace(
+            ['hin', 'dm', 'ip'],
+            ['HIN', 'DM', 'IP'],
+            str_replace( '_', ' ', $form_type )
+          )
+        )
+      );
+      $subject = sprintf( '%s_form', $form_type );
+      if( 'typist' == $db_role->name )
+      {
+        $title .= ' Entries';
+        $subject .= '_entry';
+      }
+      $this->add_menu_item( 'list', $title, $subject );
+    }
+
+    $this->add_menu_item( 'list', 'Studies', 'study' );
+  }
+
+  /**
+   * A list of all form types
+   * @var []
+   */
+  private $form_type_list = [
+    'consent', 'contact', 'hin', 'extended_hin', 'general_proxy', 'proxy', 'dm_consent', 'ip_consent'
+  ];
 }
